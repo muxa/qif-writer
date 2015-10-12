2fc06280fc1b0ff4cc5a02db956623467d70aa60
Mikhail Diatchenko <mikhail.diatchenko@gmail.com>
2015-10-12 18:13:34 +1300

    Added QIF writer; created first test

--- test/index.js
+++ test/index.js
@@ -1,10 +1,41 @@
 'use strict';
 
-var assert = require('assert');
+//var assert = require('assert');
 var qifWriter = require('../lib');
+var chai = require('chai');
+chai.use(require('chai-string'));
+var assert = chai.assert;
+var should = chai.should();
+var expect = chai.expect;
 
-describe('qif-writer', function () {
-  it('should have unit test!', function () {
-    assert(false, 'we expected this package author to add actual unit tests.');
+function BufferWriter() {
+  var self = this;
+  this.buffer = '';
+  this.write = function(s) {
+    self.buffer += s;
+  };
+}
+
+describe('qif-writer', function() {
+  var bufferWriter;
+  var transactions;
+
+  beforeEach(function() {
+    // runs before each test in this block
+    bufferWriter = new BufferWriter();
+    transactions = [];
+  });
+
+  it('should write header', function() {
+    // arrange
+
+    // act
+    qifWriter.write(transactions, {
+      output: bufferWriter.write,
+      type: 'Test'
+    });
+
+    // assert
+    assert(bufferWriter.buffer.should.startWith('!Type:Test'));
   });
 });
